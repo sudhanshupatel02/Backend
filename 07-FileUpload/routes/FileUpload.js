28f72fc3c76bbabe053e7e3c3bdca80d38cce979
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-12-29 17:19:05 +0530

    Backend 07-FileUpload

---
+++ 07-FileUpload/routes/FileUpload.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router();
+
+const {localFileUpload} = require("../controllers/fileUpload");
+
+//api route
+router.post("/localFileUpload", localFileUpload);
+
+module.exports = router;
+
+
+
+
+
+
+
