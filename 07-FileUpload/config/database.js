c38a0cc6bafce844d00dc5e8ec753282502d302d
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2023-12-29 16:40:18 +0530

    Backend 07-FileUpload

---
+++ 07-FileUpload/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+require("dotenv").config()
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL,{
+        useNewUrlParser : true,
+        useUnifiedTopology : true
+    })
+    .then(() => {
+        console.log("Database Connection established")
+    })
+    .catch((error) => {
+        // console.error(err)
+        console.log("Connection Issues with Database");
+        console.error(error);
+        process.exit(1);
+    })
+}
