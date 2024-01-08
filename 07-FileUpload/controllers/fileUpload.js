3943fc02781a1310d20d56b7566753af714d6afd
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-08 16:04:13 +0530

    cloudinary videoUpload

--- 07-FileUpload/controllers/fileUpload.js
+++ 07-FileUpload/controllers/fileUpload.js
@@ -38,7 +38,6 @@ async function uploadFileToCloudinary(file, folder){
     console.log("temp file poth", file.tempFilePath);
     return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
-
 //image upload ka hadler
 exports.imageUpload = async (req, res) =>{
     try{
@@ -78,6 +77,68 @@ exports.imageUpload = async (req, res) =>{
             success:true,
             imageUrl:response.secure_url,
             message:'Image Successfully Uploaded',
+        })
+    }
+    catch(error){
+     console.error(error);
+     res.status(400).json({
+        success:false,
+        message:'Something went wrong',
+     })
+    }
+}
+
+
+function isFileTypeSupported(type, supportedTypes){
+    return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder){
+    const options = {folder};
+    console.log("temp file poth", file.tempFilePath);
+    options.resource_type = "auto" //importent
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+//video upload ka hadler
+exports.videoUpload = async (req, res) =>{
+    try{
+        //data fetch
+        const {name, tags, email} = req.body;
+        console.log(name,tags,email);
+
+        const file = req.files.videoFile;
+        console.log(file);
+
+        //validation
+        const supportedTypes = ["mp4", "mov"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+        console.log("File Type:", fileType);
+        
+        //TODO: add a upper limit of 5mb for video
+        if(!isFileTypeSupported(fileType, supportedTypes)){
+            return res.status(400).json({
+                success:false,
+                message:'File format not supported',
+            })
+        }
+
+        //file format supported hai
+        console.log("Uploading to StudyNotion")
+        const response = await uploadFileToCloudinary(file, "StudyNotion");
+        console.log(response);
+
+        //db me entry save krni h
+        const fileData = await File.create({
+            name,
+            tags,
+            email,
+            imageUrl:response.secure_url,
+        })
+        
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:'video Successfully Uploaded',
         })
     }
     catch(error){
