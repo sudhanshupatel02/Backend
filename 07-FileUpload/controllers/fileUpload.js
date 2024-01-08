682e91c32388696ba5ba5d7125ba806dd251d2dd
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-08 12:14:40 +0530

    imageUpload

--- 07-FileUpload/controllers/fileUpload.js
+++ 07-FileUpload/controllers/fileUpload.js
@@ -1,11 +1,12 @@
 const File = require("../models/File");
+const cloudinary = require("cloudinary").v2
 
 //localfileupload -> handler function
 
 exports.localFileUpload = async (req, res) =>{
     try{
          //fetch file
-         const file = req.files.file;
+         const file = req.files.file; //file ka name file h
          console.log("FILE AAGYI JEE ->",file);
 
          //create path where file need to be stared on server
@@ -26,4 +27,41 @@ exports.localFileUpload = async (req, res) =>{
         console.log(error);
     }
 }
+
+
+function isFileTypeSupported(type, supportedTypes){
+    return supportedTypes.includes(type);
+}
+
+a
+//image upload ka hadler
+exports.imageUpload = async (req, res) =>{
+    try{
+        //data fetch
+        const {name, tags, email} = req.body;
+        console.log(name,tags,email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        //validation
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1]
+
+        if(!isFileTypeSupported(fileType, supportedTypes)){
+            return res.status(400).json({
+                success:false,
+                message:'File format not supported',
+            })
+        }
+
+        //file format supported hai
+        
+    }catch(error){
+
+    }
+}
+
+
+
 
