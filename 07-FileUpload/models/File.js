d7c3525366f50930c69f55d31391f2660e0955ba
SudhanshuPatel <sudhanshupatel0207@gmail.com>
2024-01-08 18:41:18 +0530

    email send

--- 07-FileUpload/models/File.js
+++ 07-FileUpload/models/File.js
@@ -21,6 +21,27 @@ const fileSchema = new mongoose.Schema({
 fileSchema.post("save", async function(doc){
   try{
        console.log("DOC", doc)
+
+       //transporter
+       //TODO: shift this confiquration under /config Folder 
+       let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        auth:{
+          user:process.env.MAIL_USER,
+          pass:process.env.MAIL_PASS,
+        },
+       });
+
+       //send mail
+       const info = await transporter.sendMail({
+        from: `StudyNotion`, // sender address
+        to: doc.email, // list of receivers
+        subject: "New File Uploaded on Cloudinary ✔", // Subject line
+        html: `<b>Hello world?</b> <p>File Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a> </p>`, // html body
+      });
+
+      console.log("INFO", info);
+
   }
   catch(error){
        console.error(error);
